3a1c57de4e0b67854b36467b4457e875394d37e3
Akshith Ranjan Pejavar <ranjan.akshith@gmail.com>
2017-10-06 23:49:00 +0530

    Initial Commit

---
+++ js/resume.js
@@ -0,0 +1,123 @@
+let resume={
+  "basics": {
+    "name": "AKSHITH R. PEJAVAR",
+    "label": "FRONT-END NINJA",
+    "picture": "images/profile.jpg",
+    "email": "ranjan.akshith@gmail.com",
+    "phone": "+917411774384",
+    "logo": "images/logo.png",
+    "wallpaper": "images/wallpaper.jpg",
+    "website": "",
+    "summary": "Hi, my name is Akshith Ranjan Pejavar. I am a software developer.",
+    "location": {
+      "address": "'Sri Hari Nivas', Alape-kembar, Padil P.O.",
+      "postalCode": "575007",
+      "city": "Mangalore",
+      "countryCode": "IN",
+      "region": "Karnataka"
+    },
+    "profiles": [
+      {
+        "network": "Facebook",
+        "username": "Akshith Ranjan",
+        "url": "https://www.facebook.com/akshith.ranjan.1"
+      },
+      {
+        "network": "Github",
+        "username": "Akshith-Ranjan",
+        "url": "https://github.com/Akshith-Ranjan"
+      },
+      {
+        "network": "linkedin",
+        "username": "Akshith Pejavar",
+        "url": "www.linkedin.com/in/akshith-pejavar"
+      }
+    ]
+  },
+  "projects": [
+    {
+      "title": "Facial Expression Detection using CNN",
+      "position": "Front-end and Back-end developer",
+      "website": "",
+      "picture": "images/neuron.jpg",
+      "summary": "Project about mood recognition using convolutional neural network for my college project.",
+      "source": "https://github.com/Akshith-Ranjan/Facial-Expression-Recognition-using-CNN"
+    },
+    {
+      "title": "Portfolio Site",
+      "position": "Front-end",
+      "website": "",
+      "picture": "images/portfolio.png",
+      "summary": "Portfolio site to showcase my projects.",
+      "source": "https://github.com/Akshith-Ranjan/Portfolio-Site"
+    },
+    {
+      "title": "Fresh Tomatoes Movie Trailers",
+      "position": "Front-end and Back-end developer",
+      "website": "",
+      "picture": "images/movie-trailer.png",
+      "summary": "Python server for creating static web page that display my favourite movies.",
+      "source": "https://github.com/Akshith-Ranjan/Fresh-Tomatoes-Movie-Trailers"
+    },
+    {
+      "title": "Construction Web Page",
+      "position": "Front-end developer",
+      "website": "",
+      "picture": "images/construction.png",
+      "summary": "Construction Webpage for requesting quotation and view contact information.",
+      "source": "https://github.com/Akshith-Ranjan/Construction-Web-Page"
+    }
+  ],
+  "education": [
+    {
+      "institution": "Sahyadri College of Engineering and Management",
+      "area": "Computer Science",
+      "studyType": "Bachelor of Engineering",
+      "startDate": "2013-07-01",
+      "endDate": "2017-07-01"
+    }
+  ],
+  "skills": [
+    {
+      "name": "Web Development",
+      "level": "Master",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "Javascript"
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "name": "English",
+      "level": "Fluent"
+    },
+    {
+      "name": "Hindi",
+      "level": "Fluent"
+    },
+    {
+      "name": "Kannada",
+      "level": "Fluent"
+    },
+    {
+      "name": "Malayalam",
+      "level": "Conversational"
+    }
+  ],
+  "interests": [
+    {
+      "name": "Basketball",
+      "keywords": [
+        "Sate Level player"
+      ]
+    },
+    {
+      "name": "Browsing",
+      "keywords": [
+        "Technology"
+      ]
+    }
+  ]
+};
---
+++ js/script.js
@@ -0,0 +1,135 @@
+/**
+ * @description Assign value to attribute of html elements.
+ * @param {string} identity - Class, ID , tag name
+ * @param {string} attribute - Attribute to be set
+ * @param {string} value - Value of the Attribute
+ */
+function setAttribute(identity, attribute, value) {
+	document.querySelector(identity).setAttribute(attribute, value);
+}
+
+/**
+ * @description Assign innerHTML of elements.
+ * @param {string} identity - Class, ID , tag name
+ * @param {string} text - html or text
+ */
+function setText(identity, text) {
+	document.querySelector(identity).innerHTML = text;
+}
+
+function addPopupCloseEvent() {
+	document.querySelectorAll(".close").forEach(function(btns) {
+		btns.addEventListener('click', closePopup, false);
+	});
+}
+
+/**
+* @description Shows the popup. The if else conditon check if the popup is
+required for displaying projects or user info. Accordingly the info is
+displayed in popup
+*/
+function showPopup() {
+	let dataid = this.getAttribute("data-id");
+	let heading = "",
+		image = "",
+		body = "",
+		links = "";
+	//for user info
+	if (isNaN(dataid)) {
+		heading = "About";
+		body = `<h2>${resume.basics.name}</h2>` +
+			`<p>${resume.basics.label}</p>` +
+			`<p>${resume.basics.summary}</p>`;
+		resume.basics.profiles.forEach(function(profile) {
+			links += `<a href="${profile.url}">${profile.network}</a>`;
+		});
+		//for project info
+	} else {
+		let project = resume.projects[dataid];
+		heading = project.title;
+		image = `<img class="popup-img" src="${project.picture}" alt="popup-img">`;
+		body = `<hr><p>${project.summary}</p>` +
+			`<a href="${project.source}">Source Code</a>`;
+	}
+	let html = `<div class="popup-header">` +
+		`<h2 id="popup-name">${heading}</h2>` +
+		`<i class="close material-icons">&#xE5CD;</i></div>` +
+		`<div class="popup-body">${image}${body}${links}</div>` +
+		`<div class="popup-footer">` +
+		`<button type="button" class="close close-btn" >Close</button></div>`;
+	setText(".popup", html);
+	//displaying popup
+	document.querySelector(".popup-container").style.visibility = "initial";
+	document.querySelector(".popup").style.top = "50%";
+	//adding event listeners for closing popup
+	addPopupCloseEvent();
+
+}
+
+/**
+ * @description Close the popup.
+ */
+function closePopup() {
+	let popupContainer = document.querySelector(".popup-container");
+	let popup = document.querySelector(".popup");
+	popup.style.top = "-1000px";
+	popupContainer.style.visibility = "hidden";
+}
+
+/**
+* @description Adds event listener for onclick events on project cards
+and user display picture.
+*/
+function addEvents() {
+	document.querySelectorAll(".proj").forEach(function(el) {
+		el.addEventListener('click', showPopup, false);
+	});
+	document.querySelector(".profile-pic-container").addEventListener(
+		'click', showPopup, false);
+}
+
+/**
+* @description Display all projects present in the resume.js file.
+* @param {object} project - project object contains details like title, link,
+discreption etc.
+* @return {string} String consisting of all the project info embedded in tags.
+*/
+function createProjectHTML(project) {
+	let projectHTML = `<div class = "project"><div class = "card">` +
+		`<div class ="proj" data-id = "${resume.projects.indexOf(project)}">` +
+		`<div class = "shaper">` +
+		`<img src = "${project.picture}" alt = "${project.title}">` +
+		`</div><h2 class = "project-name">${project.title}</h2></div>` +
+		`<a  href = "${project.source}">Source Code</a></div></div>`;
+	return projectHTML;
+}
+
+/**
+ * @description Display all projects present in the resume.js file.
+ */
+function displayAllProjects() {
+	let projectHTML = "";
+	resume.projects.forEach(function(project) {
+		projectHTML += createProjectHTML(project);
+	})
+	setText(".project-container", projectHTML);
+}
+
+/*
+function setProfileInfo(){
+  let basicInfo = resume.basics;
+  setAttribute(".logo", "src", basicInfo.logo);
+  setText(".name", basicInfo.name);
+  setText(".label", basicInfo.label);
+  setAttribute(".wallpaper-pic", "src", basicInfo.wallpaper);
+  setAttribute(".profile-pic", "src", basicInfo.picture);
+}
+*/
+
+function main() {
+	//setProfileInfo();
+	displayAllProjects();
+	addEvents();
+}
+
+main();
